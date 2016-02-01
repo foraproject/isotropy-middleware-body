3b475da40a2c919b3c7843b43f1aef22ce45acc5
Jeswin Kumar <jeswinpk@agilehead.com>
2016-02-01 20:47:26 +0530

    Fix type annotations

--- src/isotropy-middleware-body.js
+++ src/isotropy-middleware-body.js
@@ -2,7 +2,7 @@
 import isotropyBody from "isotropy-body";
 import type { IncomingMessage, ServerResponse } from "./flow/http-types";
 
-export default async function(req: IncomingMessage, res: ServerResponse, options: Object) : Promise {
+export default async function(req: IncomingMessage, res: ServerResponse, options?: Object) : Promise {
   try {
     req.body = await isotropyBody(req, options);
   } catch (e) {
