ef8344d70733597648ffca77980551d04bfb72b5
Jeswin Kumar <jeswinpk@agilehead.com>
2016-02-01 12:29:15 +0530

    If we can't parse, body must be set to {}

--- src/isotropy-middleware-body.js
+++ src/isotropy-middleware-body.js
@@ -3,5 +3,9 @@ import isotropyBody from "isotropy-body";
 import type { IncomingMessage, ServerResponse } from "./flow/http-types";
 
 export default async function(req: IncomingMessage, res: ServerResponse, options: Object) : Promise {
-  req.body = await isotropyBody(req, options);
+  try {
+    req.body = await isotropyBody(req, options);
+  } catch (e) {
+    req.body = {};
+  }
 }
--- src/test/test.js
+++ src/test/test.js
@@ -12,14 +12,16 @@ describe('Isotropy Parse Url', () => {
         parse(req).then(() => {
           req.body.foo.bar.should.equal('baz');
           res.end();
-        });
+          resolve();
+        })
+        .catch((err) => reject(err));
       });
 
       return request(server)
         .post('/')
         .type('form')
         .send({ foo: { bar: 'baz' }})
-        .end(() => resolve());
+        .end();
     });
   });
 
@@ -29,13 +31,15 @@ describe('Isotropy Parse Url', () => {
         parse(req).then(() => {
           req.body.should.eql({ foo: 'bar' });
           res.end();
-        });
+          resolve();
+        })
+        .catch((err) => reject(err));
       });
 
       return request(server)
       .post('/')
       .send({ foo: 'bar' })
-      .end(() => resolve());
+      .end();
     });
   });
 
@@ -45,14 +49,36 @@ describe('Isotropy Parse Url', () => {
         parse(req).then(() => {
           req.body.should.eql('hello world');
           res.end();
-        });
+          resolve();
+        })
+        .catch((err) => reject(err));
       });
 
       return request(server)
       .post('/')
       .set('content-type', 'text/plain')
       .send('hello world')
-      .end(() => resolve());
+      .end();
+    });
+  });
+
+
+  it('should be empty if there is an error', function() {
+    return new Promise((resolve, reject) => {
+      const server = http.createServer((req, res) => {
+        parse(req).then(() => {
+          req.body.should.deepEqual({});
+          res.end();
+          resolve();
+        })
+        .catch((err) => reject(err));
+      });
+
+      return request(server)
+      .post('/')
+      .set('content-type', 'invalid/type')
+      .send('hello world')
+      .end();
     });
   });
 
